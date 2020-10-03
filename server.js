d341d887bc0bd89f7bcf230fb3277b8e8e9d8aec
Eugene Lepeico <eugenelepeico@gmail.com>
2020-10-03 13:27:49 +0300

    fix(port): change port to process.env.PORT

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
 const server = require('http').createServer();
 const router = require('./lib/router');
 
-server.on('request', (req, res) => { router(req, res, server); }).listen(8000);
+server.on('request', (req, res) => { router(req, res, server); }).listen(process.env.PORT || 8000);
