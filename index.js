5f431a37f858b3f7ded617d2707b4d6d9d5aa1ef
Eugene Lepeico <eugenelepeico@gmail.com>
2020-10-03 22:55:12 +0300

    feat(client): enterRoom & say methods

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const EventEmitter = require('events');
+const http = require('http');
 
 class P2P extends EventEmitter {
   constructor(host) {
@@ -6,12 +7,42 @@ class P2P extends EventEmitter {
     if (!host) throw new Error('no host specified');
 
     this.host = host;
-    this.subscribers = new Map();
+    this.rooms = new Map();
   }
 
-  subscribe(channel) {}
+  enterRoom(roomName) {
+    const room = http.request({
+      hostname: this.host,
+      port: 8000,
+      path: `/rooms/${roomName}`,
+      method: 'GET',
+    }).end();
 
-  send(channel, message = {}) {}
+    this.rooms.set(roomName, room);
+
+    room.once('close', () => {
+      console.log('bye');
+      delete this.rooms[roomName];
+      room.destroy();
+    });
+
+    return room;
+  }
+
+  say(roomName, message = {}) {
+    const res = http.request({
+      hostname: this.host,
+      port: 8000,
+      path: `/rooms/${roomName}`,
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Content-Length': message.length,
+      },
+    })
+    res.write(message);
+    res.end();
+  }
 }
 
 module.export = P2P;
